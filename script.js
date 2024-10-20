57b958d7562e2ed91284979e80e21707dd4d144f
Shubhpreet-kaur <121420996+Shubhpreet-kaur@users.noreply.github.com>
2024-10-20 05:30:57 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ script.js
@@ -0,0 +1,92 @@
+const arr=[
+    {
+       name:"probability_and_statistics",
+       title:[
+        {"UNIT_1":"Sample space, probability  axioms, conditional probability, Bayes' theorem and independence of events. Concept of real random variables (discrete and continuous), cumulative distribution function, probability mass/density functions. Functions of a random variable."},
+        {"UNIT_2":"Mathematical expectation, moments, moment generating function, characteristic function. Moment inequalities: Markov inequality, Chebyshev-Bienayme inequality, Lyapunov inequality. Multiple random variables, independent random variables, joint cumulative distribution function, joint probability mass/density functions, marginal probability mass/density functions. Functions of several random variables. Order statistics and their distributions."},
+        {"UNIT_3":"Covariance, correlation, joint moments and conditional expectational. Discreate distributions and their interrelations: Degenerate, two-point, uniform, Bernoulli, binomial, geometric (case 1 and case 2),negative binomial (case 1, 2 and 3), hypergeometric, Poisson."},
+        {"UNIT_4":"Continuous distributions and their interrelations: uniform, exponential, Erlang (k), Gamma, beta, normal, lognormal, Cauchy, Weibull, Laplace. Statement and interpretation of weak law of large numbers and strong law of large numbers, central limit theorem for independent and identically distributed random variables with finite variance."}
+       ]
+
+    },
+    {
+        name:"mathematical_modeling",
+        title:[
+         {"UNIT_1":"Introduction to mathematical modelling, modelling approaches: (Empirical, Stochastic, Simulation, Deterministic, and Statistical models), Classifications and some characteristics of Mathematical Modelling, limitations of Mathematical Modelling.<br/><br/><b>Modelling of Differential Equations: </b> <br/>Compartmental models: Exponential decay and radioactivity, detecting art forgeries, lake pollution models (case study of lake burley griffin), Drug assimilation into the blood (a single fast-dissolving cold pill, course of slowly dissolving cold pills), equilibrium points and stability, Models of single populations, Density-dependent growth, Limited growth with harvesting, Discrete population growth and chaos, Time-delayed regulation, The Differential Equation of the Vibrations of a Mass on a Spring, Free, Undamped Motion, Free Damped Motion, Forced Motion, Resonance Pheonomena, Electric Circuit Problems."},
+         {"UNIT_2":"Interacting population models: Model for an influenza outbreak, Cholera, Predators and prey, Competing species, Model of a battle, Analysis of a battle model, Analysis of a predator-prey model, Analysis of competing species models. <br/>  Laplace transform and inverse transform application to initial value problem with constant coefficients and with discontinuous non homogeneous terms."},
+         {"UNIT_3":"<b>Heat and mass transport models:</b><br/> Newton's law of cooling, water heater, heat conduction and Fourier's law, heat conduction through a wall, radial heat conduction, diffusion.<br/><b> Time dependent heat models: </b><br/>case studies of cooling, water heater, heat loos through wall, double glazing, cooling computer chip and tumor growth."},
+         {"UNIT_4":"<b>Monte Carlo Simulation Modelling:</b><br/>simulating deterministic behaviour (area under a curve, volume under a surface),<br/> <b>Generating Random Numbers: </b> <br/>  middle square method, linear congruence.<br/><b> Monte Carlo Simulation Modelling: </b>(probabilistic)<br/> detecting fair and unfair coin, dice, <br/><b>Inventory model: </b>(Gasoline and consumer demand). Harbor system and morning rush hour modelling. <br/> <b> Discrete probabilistic modeling: </b> Discrete systems for transition matrix, system reliability, linear regression model (with case studies)"}
+        ]
+ 
+     },
+     {
+        name:"statics",
+        title:[
+         {"UNIT_1":"<b>Basic notions:</b><br/> Inertial and non-inertial frame of reference, Weight of body, Force, Force with contact and without contact, Force systems, Principle of transmissibility of forces, Basic concepts of mechanics.<br/><b> Forces acting on a particle: </b><br/>Parallelogram law of forces, Triangle law of forces and its converse, Polygon of forces, λ − µ theorem, Lami’s theorem and its converse, Components of a force in given directions. Resolution of forces, Theorem on resolved parts of two and more concurrent forces. Condition of equilibrium of any number of forces, Trignometric m − n theorem, equilibrium of a rigid body under the action of three forces. "},
+         {"UNIT_2":"<b>Parallel forces:</b><br/> Resultant of two like parallel forces, unequal unlike parallel forces, Theorem of resolved parts of two parallel forces, Centre of parallel forces, Centre of gravity. <br/><b>Moments and Couples:</b><br/> Moment of a force about a point, Moment of a force about a line; Couple, Moment of a couple, Varignon’s theorem on moments of two coplanar forces. Composition of coplanar couples, Composition of a number of couples, equilibrium of couples, equivalence of couples."},
+         {"UNIT_3":"<b>Coplanar forces: </b><br/>Resultant of a system of coplanar forces, Resultant of three coplanar forces to two, Reduction of any number of coplanar forces to a single force or a single couple, Generalization theorem of resolved parts, generalisation of Varignon’s theorem of moments, Condition of equilibrium of a system of coplanar forces, Reduction of two coplanar forces to a single force or a single couple Resultant of a force and a couple, Resolution of a force into a force and a couple, Reduction of a system of coplanar forces to a force and a couple."},
+         {"UNIT_4":"<b>Friction: </b><br/>Definition and nature of friction, coefficient of friction, angle of friction, cone of friction, laws of friction, equilibrium of a particle on a rough plane, Problems on ladders, rods etc. <br/><b>Virtual Work:</b><br/> Work done by a force, Principle of virtual work with Applications."}
+        ]
+ 
+     },
+     {
+        name:"group_theory",
+        title:[
+         {"UNIT_1":"Group actions, Group acting on themselves by left multiplication and conjugation, Stabilizers, orbits and kernels, Orbit-Stabilizer relation, Permutation representation associated with a given group action."},
+         {"UNIT_2":"Applications of group actions, Generalized Cayley’s theorem, Index theorem, Class equation and its applications., Simplicity of An, n > 4."},
+         {"UNIT_3":"p-Groups, Sylow’s theorems and its applications, Semidirect products, Groups of order p2, p3 and pq, Classification of groups of order upto 15."},
+         {"UNIT_4":"Normal and subnormal series, Derived series, composition series, solvable groups and nilpotent groups, Zassenhaus lemma, Schreier refinement theorem, Jordan Holder’s theorem."}
+        ]
+ 
+     },
+     {
+        name:"multivariate_calculus",
+        title:[
+         {"UNIT_1":"Directional derivatives, total differentiability, Jacobian, Chain rule, Mean value theorem for differentiable functions, sufficient condition for differentiability, symmetry of mixed partial derivatives, Taylor’s formula for real valued functions of several variables."},
+         {"UNIT_2":"The gradient, maximal and normal property of the gradient, tangent planes, Extrema of functions of several variables, method of Lagrange multipliers, constrained optimization problems, convex functions, the inverse function theorem and the implicit function theorem."},
+         {"UNIT_3":"Integration over a rectangle, Jordan regions, the integral over a Jordan region, iterated integrals, the change of variables formula"},
+         {"UNIT_4":"1-Forms and path Integrals, change of variables, differential forms of higher order, Green’s theorem,surface integrals and Stokes’s theorem, Gauss’s theorem, chains and cycles, the divergence theorem"}
+        ]
+ 
+     }
+]
+const btn=document.getElementById('search-btn');
+const input_box=document.getElementById('inp-box');
+const result=document.querySelector('.result');
+btn.addEventListener("click",insertText);
+input_box.addEventListener('mousedown',addDisable);
+function addDisable(){
+    document.getElementById('default').setAttribute('disabled','true');
+}
+function insertText() {
+    var input_val=input_box.value;
+    console.log(input_val)
+    if(input_val==''){
+        result.innerHTML="THERE IS AN ERROR";
+    }
+    else{
+        arr.forEach(ele=> {
+            if(ele.name==input_val){
+                result.innerHTML=`
+                <div class='boxes'>
+                    <b class="unit">UNIT 1 : </b>
+                    <p class="text">${ele.title[0].UNIT_1}</p>
+                </div>
+                <div class='boxes'>
+                    <b class="unit">UNIT 2 : </b>
+                    <p class="text">${ele.title[1].UNIT_2}</p>
+                </div>
+                <div class='boxes'>
+                    <b class="unit">UNIT 3 : </b>
+                    <p class="text">${ele.title[2].UNIT_3}</p>
+                </div>
+                <div class='boxes'>
+                    <b class="unit">UNIT 4 : </b>
+                    <p class="text">${ele.title[3].UNIT_4}</p>
+                </div>
+                `;
+                
+            }
+        });
+    }
+}
